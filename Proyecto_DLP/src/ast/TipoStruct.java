16fb490fb84152e277497e9e5d95860445737044
Samuel Moreno Vincent <UO266321@uniovi.es>
2019-04-04 09:57:09 +0200

    Solucionado visitar struct no instanciados

--- Proyecto_DLP/src/ast/TipoStruct.java
+++ Proyecto_DLP/src/ast/TipoStruct.java
@@ -53,7 +53,6 @@ public class TipoStruct extends AbstractTipo {
 				Definicion_campo_struct def = this.definicion.getDefinicion_campo_struct().get(i);
 				def.setAddress(this.size);
 				this.size += def.getTipo().getSize();
-				//System.out.println(this.size + " " + def.getNombre()); //TODO borrar
 			}
 		}
 
