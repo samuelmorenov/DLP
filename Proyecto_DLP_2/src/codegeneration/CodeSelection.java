e1c2f9c55e29a18b13f0f8bae009e614b44c2057
samuelmorenov <UO266321@uniovi.es>
2020-04-21 23:44:22 +0200

    Añadidos todos los codeselection (provisional)

--- Proyecto_DLP_2/src/codegeneration/CodeSelection.java
+++ Proyecto_DLP_2/src/codegeneration/CodeSelection.java
@@ -1,13 +1,7 @@
-/**
- * Tutorial de Diseño de Lenguajes de Programación
- * @author Raúl Izquierdo
- */
-
 package codegeneration;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import ast.*;
 import visitor.*;
@@ -17,6 +11,8 @@ enum CodeFunction {
 }
 
 public class CodeSelection extends DefaultVisitor {
+
+	private Map<String, String> instruccion = new HashMap<String, String>();
 
 	public CodeSelection(Writer writer, String sourceFile) {
 		this.writer = new PrintWriter(writer);
@@ -37,26 +33,39 @@ public class CodeSelection extends DefaultVisitor {
 		instruccion.put("<=", "le");
 		instruccion.put("==", "eq");
 		instruccion.put("!=", "ne");
-	}
 
-	// # ----------------------------------------------------------
-	/*
-	 * Poner aquí los visit.
-	 *
-	 * Si se ha usado VGen, solo hay que copiarlos de la clase
-	 * 'visitor/_PlantillaParaVisitors.txt'.
-	 */
+	}
 
 	public Object visit(Program node, Object param) {
 		out("#source \"" + sourceFile + "\""); // #SOURCE {file}
-		visitChildren(node.getDefinicion(), param);// define[[definicioni]]
+		out("call main");// CALL main
+		out("halt");// HALT
+		visitChildren(node.getDefinicion(), param);// define[[bloquei]]
 
 		return null;
 	}
 
 	public Object visit(Definicion_funcion node, Object param) {
 
-		super.visit(node, param);// ejecuta[[sentenciasi]]
+		int localesSize = 0;
+		int parametrosSize = 0;
+		int retornoSize = 0;
+		retornoSize = node.getRetorno().getSize();
+		for (Definicion_variable e : node.getLocales()) {
+			localesSize += e.getTipo().getSize();
+		}
+		for (Definicion_variable e : node.getParametros()) {
+			parametrosSize += e.getTipo().getSize();
+		}
+
+		out(node.getNombre() + ":");// {nombre}:
+		out("enter " + localesSize);// ENTER {sumatorio localesi.tipo.size}
+		visitChildren(node.getSentencias(), param);// ejecuta[[sentenciasi]]
+
+		if (node.getRetorno() instanceof Tipo_Void)// si retorno == VOID
+			out("ret " + retornoSize + ", " + localesSize + ", " + parametrosSize);
+		// RET {retorno.size}, {sumatorio localesi.tipo.size}, {sumatorio
+		// parametrosi.tipo.size}
 
 		return null;
 	}
@@ -71,29 +80,98 @@ public class CodeSelection extends DefaultVisitor {
 
 	public Object visit(Sentencia_print node, Object param) {
 		line(node);// #LINE {end.line}
-		super.visit(node, param); // value[[expresiones]]
+		super.visit(node, CodeFunction.VALUE); // value[[expresiones]]
 		out("out", node.getExpresiones().getTipo()); // OUT<expresiones.tipo>
+		if (node.getFincadena().equals("sp")) {
+			out("pushb 32"); // PUSHB [[finCadena]]
+			out("outb"); // OUTB
+		}
+		if (node.getFincadena().equals("ln")) {
+			out("pushb 10");// PUSHB [[finCadena]]
+			out("outb");// OUTB
+		}
 		return null;
 	}
 
-	// class Expr_int { String string; }
+	public Object visit(Sentencia_read node, Object param) {
+		line(node);// #LINE {end.line}
+		super.visit(node, CodeFunction.VALUE);// value[[expresiones]]
+		out("in", node.getExpresiones().getTipo());// IN<expresiones.tipo>
+		return null;
+	}
+
+	public Object visit(Sentencia_if node, Object param) {
+		line(node);// #LINE {end.line}
+		String contadorIf = String.valueOf(++contadorGeneralIF);// {contadorIF = ++contadorGeneralIF}
+		node.getCondicion().accept(this, param);// valor[[condicion]]
+		out("jz else" + contadorIf);// jz else{contadorIf}
+		visitChildren(node.getSentencias(), param);// ejecuta[[sentenciasi]]
+		out("jmp finElse" + contadorIf);// jmp finIf{contadorIf}
+		out("else" + contadorIf + ":");// else{contadorIf}:
+		visitChildren(node.getSino(), param);// ejecuta[[sinoi]]
+		out("finElse" + contadorIf + ":");// finIf{contadorIf}:
+		return null;
+	}
+
+	public Object visit(Sentencia_while node, Object param) {
+		line(node);// #LINE {end.line}
+		String contadorWhile = String.valueOf(++contadorGeneralWhile);// {contadorWhile = ++contadorGeneralWhile}
+		out("while" + contadorWhile + ":");// while{contadorWhile}:
+		node.getCondicion().accept(this, param);// valor[[condicion]]
+		out("jz finWhile" + contadorWhile);// jz finWhile{contadorWhile}
+		visitChildren(node.getSentencias(), param);// ejecuta[[sentenciasi]]
+		out("jmp while" + contadorWhile);// jmp while{contadorWhile}
+		out("finWhile" + contadorWhile + ":");// finWhile{contadorWhile}:
+		return null;
+	}
+
+	public Object visit(Sentencia_llamada_funcion node, Object param) {
+		line(node);// #LINE {end.line}
+		visitChildren(node.getParametros(), param);// valor[[parametrosi]]
+		out("call " + node.getNombre());// CALL {nombre}
+		if (!(node.getDefinicion().getRetorno() instanceof Tipo_Void)) {// si
+																		// sentencia_llamada_funcion.definicion.retorno
+																		// != tipoVoid
+			out("pop", node.getDefinicion().getRetorno());// POP< sentencia_llamada_funcion.definici�n.retorno>
+		}
+		return null;
+	}
+
+	public Object visit(Sentencia_return node, Object param) {
+		int retorno = 0;
+		int localesi = 0;
+		int parametrosi = 0;
+		retorno = node.getFuncionActual().getRetorno().getSize();
+		for (Definicion_variable child : node.getFuncionActual().getLocales())
+			localesi = localesi + child.getTipo().getSize();
+		for (Definicion_variable child : node.getFuncionActual().getParametros())
+			parametrosi = localesi + child.getTipo().getSize();
+
+		if (node.getExpresion() != null) {// si expresion != null
+			line(node);// #LINE {end.line}
+			node.getExpresion().accept(this, param);// valor[[expr]]
+		}
+		out("ret " + retorno + // RET {sentencia_return.funcion.retorno.size},
+				", " + localesi + // {sumatorio sentencia_return.funcion.localesi.tipo.size},
+				", " + parametrosi);// {sumatorio sentencia_return.funcion.parametrosi.tipo.size}
+		return null;
+	}
+
 	public Object visit(Expr_int node, Object param) {
 		assert (param == CodeFunction.VALUE);
-		out("pushi " + node.getString()); // PUSHI {value}
+		out("push " + node.getString()); // PUSH {value}
 		return null;
 	}
 
-	// class Expr_real { String string; }
 	public Object visit(Expr_real node, Object param) {
 		assert (param == CodeFunction.VALUE);
 		out("pushf " + node.getString());// PUSHF {value}
 		return null;
 	}
 
-	// class Expr_char { String string; }
 	public Object visit(Expr_char node, Object param) {
 		assert (param == CodeFunction.VALUE);
-		out("pushb " + node.getString());// PUSHB {value}
+		out("pushb " + (int) node.getString().charAt(1));// PUSHB {value}
 		return null;
 	}
 
@@ -102,37 +180,63 @@ public class CodeSelection extends DefaultVisitor {
 		if (((CodeFunction) param) == CodeFunction.VALUE) {
 			visit(node, CodeFunction.ADDRESS); // address [[expr_ident]]
 			out("load", node.getTipo()); // LOAD< expr_ident.type>
-		} else { // Funcion.DIRECCION
+		}
+
+		else { // Funcion.DIRECCION
 			assert (param == CodeFunction.ADDRESS);
 
-			out("pusha " + node.getDefinicion().getAddress());
+			if (node.getDefinicion().getAmbito() == Ambito.GLOBAL) {
+				out("pusha " + node.getDefinicion().getAddress());
+			}
+
+			if (node.getDefinicion().getAmbito() == Ambito.LOCAL) {
+				out("pusha BP");
+				out("push " + node.getDefinicion().getAddress());
+				out("add");
+			}
+
 		}
 		return null;
 	}
 
 	public Object visit(Expr_operacion node, Object param) {
 		assert (param == CodeFunction.VALUE);
-		node.getIzquierda().accept(this, CodeFunction.VALUE);
-		node.getDerecha().accept(this, CodeFunction.VALUE);
-		out(instruccion.get(node.getOperador().getString()), node.getTipo());
+		node.getIzquierda().accept(this, CodeFunction.VALUE);// value[[izquierda]]
+		node.getDerecha().accept(this, CodeFunction.VALUE);// value[[derecha]]
+		out(instruccion.get(node.getOperador().getString()), node.getIzquierda().getTipo());
 		return null;
 	}
 
 	public Object visit(Expr_acceso_vector node, Object param) {
 		node.getFuera().accept(this, CodeFunction.ADDRESS); // address[[fuera]]
 		node.getDentro().accept(this, CodeFunction.VALUE); // value[[dentro]]
-		out("pusha " + node.getTipo().getSize()); // PUSHA {tipo.size}
+		out("push " + node.getTipo().getSize()); // PUSHA {tipo.size}
 		out("mul");// MUL
 		out("add");// ADD
+
+		if (((CodeFunction) param) == CodeFunction.VALUE) {
+			out("load", node.getTipo()); // LOAD{tipo.size}
+		}
 		return null;
 	}
 
-//	public Object visit(Expr_acceso_struct node, Object param) {
-//		node.getIzquierda().accept(this, CodeFunction.ADDRESS); // address[[izquierda]]
-//		node.getDerecha().accept(this, CodeFunction.ADDRESS); // address[[derecha]]
-//		out("add");// ADD
-//		return null;
-//	}
+	public Object visit(Expr_negada node, Object param) {
+		assert (param == CodeFunction.VALUE);
+		node.getDerecha().accept(this, CodeFunction.VALUE);// value[[derecha]]
+		out(instruccion.get(node.getOperador().getString()), node.getDerecha().getTipo());// {operador.instruccion}
+		return null;
+	}
+
+	public Object visit(Expr_acceso_struct node, Object param) {
+		node.getStruct().accept(this, CodeFunction.ADDRESS); // address[[struct]]
+		out("push " + node.getTipo().getSize()); // PUSH {tipo.size} //TODO comprobar esto
+		out("add");// ADD
+
+		if (((CodeFunction) param) == CodeFunction.VALUE) {
+			out("load", node.getTipo()); // LOAD< expr_ident.type> //TODO comprobar esto
+		}
+		return null;
+	}
 
 	public Object visit(Expr_cast node, Object param) {
 		node.getExpr().accept(this, CodeFunction.VALUE); // value[[expr]]
@@ -142,12 +246,18 @@ public class CodeSelection extends DefaultVisitor {
 		return null;
 	}
 
-	// # ----------------------------------------------------------
-	// Métodos auxiliares recomendados (opcionales) -------------
+	public Object visit(Expr_llamada_funcion node, Object param) {
+		for (Expr child : node.getParametros())
+			child.accept(this, param);// valor[[parametrosi]]
+		out("call " + node.getNombre());// CALL {nombre}
+		return null;
+	}
+	//////////////////////////////////////////
 
-	// Imprime una línea en el fichero de salida
 	private void out(String instruction) {
 		writer.println(instruction);
+		// System.out.println(instruction); // TODO - Comentar para que no se imprima
+		// por pantalla la generacion de codigo
 	}
 
 	private void out(String instruccion, Tipo tipo) {
@@ -158,9 +268,6 @@ public class CodeSelection extends DefaultVisitor {
 		}
 	}
 
-	// Imprime la directiva `#line` junto con la línea del fichero de entrada donde
-	// estaba el nodo
-	// indicado
 	private void line(AST node) {
 		line(node.getEnd());
 	}
@@ -171,5 +278,6 @@ public class CodeSelection extends DefaultVisitor {
 
 	private PrintWriter writer;
 	private String sourceFile;
-	private Map<String, String> instruccion = new HashMap<String, String>();
+	private int contadorGeneralIF = 0;
+	private int contadorGeneralWhile = 0;
 }
